7fb3b95487678276d260fae0c7e4cd68c22ace44
yoohooitstoo <stumanscott@gmail.com>
2020-06-05 12:53:47 -0400

    added variable confirms

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-
-
-var lowerCase = [a-z];
-var numbers = [0-9];
-var upperCase = [A-Z];
-var symbols = [ ];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "*", "_", "+", "~", "|", "?","-", "=", "?"];
 var userPassword = [];
 
+var upper = confirm("would you like to use upper case letters?")
+var lower = confirm("would you like to use lower case letters?")
+var number = confirm("would you like to use numbers?")
+var specials = confirm("would you like to use special characters?")
+
+var userChoices = [];
+
+if (upper && lower && number && special === true) {
+  userChoices = userChoices.concat[];
+}
 
 
 function writePassword() {
