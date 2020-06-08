ab9fd91459c13fccf52b99ee3ab4c047077aca91
yoohooitstoo <stumanscott@gmail.com>
2020-06-08 06:12:04 -0400

    for loop math random

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //This function allows you to generate a random password based on the users choices and password length
-function generatePassword()
-{
+function generatePassword() {
   //variables needed to generate a password
   validNumbers = "0123456789";
   validLowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -12,56 +11,51 @@ function generatePassword()
   validSpecialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
   //make sure that the password length is correct
-  
+
   passwordLength = 0;
 
-  while(true)
-  {
+  while (true) {
     passwordLength = prompt("Enter your password Length. (Must be between 8 and 128");
-    if (passwordLength >= 8 && passwordLength <= 128){
-        break;
-    }else{
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      break;
+    } else {
       alert("Must be a number between 8 and 128")
     }
   }
 
   // Confirm user wants to use certain values answer should show in box
-  while(true)
-  {
+  while (true) {
     //valid Characters is our final result
+   // result = "";
     validCharacters = "";
-    if(confirm("Would you like to include numbers in your password?"))
-    {
-      validCharacters+=validNumbers;
+    if (confirm("Would you like to include numbers in your password?")) {
+      validCharacters += validNumbers;
     }
-    if(confirm("Would you like to include lower case in your password?"))
-    {
-      validCharacters+=validLowerCase;
+    if (confirm("Would you like to include lower case in your password?")) {
+      validCharacters += validLowerCase;
     }
-    if(confirm("Would you like to include upper case in your password?"))
-    {
-      validCharacters+=validUpperCase;
+    if (confirm("Would you like to include upper case in your password?")) {
+      validCharacters += validUpperCase;
     }
-    if(confirm("Would you like to include special characters in your password?"))
-    {
-      validCharacters+=validSpecialCharacters;
+    if (confirm("Would you like to include special characters in your password?")) {
+      validCharacters += validSpecialCharacters;
     }
-    if(validCharacters.length === 0)
-    {
+    if (validCharacters.length === 0) {
       alert("Please pick one.")
-    }
-    else
-    {
+    } else {
       break;
     }
   }
 
+  for (i = 0; i < passwordLength; i++) {
+    randomIndex = Math.floor(Math.random() * validCharacters.length);
+    randomCharacter = validCharacters[randomIndex];
+    //result = result + randomCharacter;
+  }
 
-
-
-
+  return randomIndex;
 }
-return validCharacters;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -104,7 +98,7 @@ generateBtn.addEventListener("click", writePassword);
 
 //   }else if
 //   (upper && lower === true)
-  
+
 
 // console.log(userChoices);
 
